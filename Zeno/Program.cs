30cd0e6bb1f26fdde5f24cc535922b640190db97
pedro-henrique06 <pedropuduia@hotmail.com>
2026-04-27 02:17:12 -0300

    fix: Improve database connection string logging in Program.cs

--- Zeno/Program.cs
+++ Zeno/Program.cs
@@ -40,7 +40,9 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 builder.Services.AddValidatorsFromAssemblyContaining<Zeno.Application.Validators.EntryValidator>();
-builder.Services.AddInfrastructureSQL(builder.Configuration["Database:ConnectionString"]!);
+var connStr = builder.Configuration["Database:ConnectionString"]!;
+Console.WriteLine($"[DEBUG] ConnectionString: {connStr}");
+builder.Services.AddInfrastructureSQL(connStr);
 builder.Services.AddScoped<IEntryService, EntryService>();
 builder.Services.AddScoped<IWalletService, WalletService>();
 builder.Services.AddScoped<IHomeService, HomeService>();
